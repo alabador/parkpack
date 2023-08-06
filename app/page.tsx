22e34b82db1fc4d09faf4229bb8c66101821c306
Austin Labador <austinlabador@gmail.com>
2023-08-06 03:16:56 -0700

    Create hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from '@/components/Hero'
 import ParkCard from '@/components/ParkCard'
 
 export default function Home() {
   return (
     <main className="min-h-screen ">
-      
+      <Hero></Hero>
     </main>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,12 @@
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section 
+        className="h-[70vh] bg-[url(/yosemite-hero.jpg)] bg-no-repeat bg-left-top
+        text-white flex items-center pl-8 lg:pl-16 pr-4 bg-slate-400 bg-blend-multiply ">
+        <div className="max-w-[60ch]">
+            <h1 className="text-6xl font-bold">Discover America's National Parks</h1>
+            <h2 className="mt-8 text-lg">Plan your next trip to one of over 400+ national parks in the United States. </h2>
+        </div>
+    </section>
+  );
 }
