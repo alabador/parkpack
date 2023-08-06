0b123a85b012f1bd6e487fc2f981a4d9d5e6b6e8
Austin Labador <austinlabador@gmail.com>
2023-08-06 03:47:31 -0700

    Add searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Hero from '@/components/Hero'
+import Hero from '@/app/components/Hero'
 import ParkCard from '@/components/ParkCard'
+import Searchbar from '@/components/Searchbar'
 
 export default function Home() {
   return (
     <main className="min-h-screen ">
       <Hero></Hero>
+      <Searchbar />
     </main>
   )
 }
---
+++ components/Searchbar.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { Button, FormControl, Input, Select } from "@chakra-ui/react";
+
+export default function Searchbar() {
+  return (
+    <div className="p-8">
+        <FormControl className="
+            p-8 rounded-md relative bottom-24 bg-white flex gap-4 flex-col md:flex-row
+        ">
+            <Select>
+                <option value="AL">Alabama</option>
+                <option value="AK">Alaska</option>
+                <option value="AZ">Arizona</option>
+                <option value="AR">Arkansas</option>
+                <option value="CA">California</option>
+                <option value="CO">Colorado</option>
+                <option value="CT">Connecticut</option>
+                <option value="DE">Delaware</option>
+                <option value="DC">District Of Columbia</option>
+                <option value="FL">Florida</option>
+                <option value="GA">Georgia</option>
+                <option value="HI">Hawaii</option>
+                <option value="ID">Idaho</option>
+                <option value="IL">Illinois</option>
+                <option value="IN">Indiana</option>
+                <option value="IA">Iowa</option>
+                <option value="KS">Kansas</option>
+                <option value="KY">Kentucky</option>
+                <option value="LA">Louisiana</option>
+                <option value="ME">Maine</option>
+                <option value="MD">Maryland</option>
+                <option value="MA">Massachusetts</option>
+                <option value="MI">Michigan</option>
+                <option value="MN">Minnesota</option>
+                <option value="MS">Mississippi</option>
+                <option value="MO">Missouri</option>
+                <option value="MT">Montana</option>
+                <option value="NE">Nebraska</option>
+                <option value="NV">Nevada</option>
+                <option value="NH">New Hampshire</option>
+                <option value="NJ">New Jersey</option>
+                <option value="NM">New Mexico</option>
+                <option value="NY">New York</option>
+                <option value="NC">North Carolina</option>
+                <option value="ND">North Dakota</option>
+                <option value="OH">Ohio</option>
+                <option value="OK">Oklahoma</option>
+                <option value="OR">Oregon</option>
+                <option value="PA">Pennsylvania</option>
+                <option value="RI">Rhode Island</option>
+                <option value="SC">South Carolina</option>
+                <option value="SD">South Dakota</option>
+                <option value="TN">Tennessee</option>
+                <option value="TX">Texas</option>
+                <option value="UT">Utah</option>
+                <option value="VT">Vermont</option>
+                <option value="VA">Virginia</option>
+                <option value="WA">Washington</option>
+                <option value="WV">West Virginia</option>
+                <option value="WI">Wisconsin</option>
+                <option value="WY">Wyoming</option>
+            </Select>
+            <Input placeholder="Search for national park"/>
+            <Button size={'md'} className="px-8 py-4 bg-green-800 text-white">Search</Button>
+        </FormControl>
+    </div>
+  )
+}
