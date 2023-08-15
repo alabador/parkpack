bef160f80b464f756eccf872facf3efde6807b81
Austin Labador <austinlabador@gmail.com>
2023-08-15 03:08:20 -0700

    Implement api fetch into search section, remove state component from url query

--- app/parks/components/SearchResults.tsx
+++ app/parks/components/SearchResults.tsx
@@ -1,11 +1,26 @@
+import GetData from "@/api/GetData"
 
+
+interface parkInfo {
+  total: string
+  limit: string
+  start: string
+  data: [
+    {
+      fullName: string
+    }
+  ]
+
+}
 
 export default async function SearchResults({stateSearch, parkSearch}: 
     {stateSearch:string, parkSearch: string}) {
-  return (
-    <div>
-        <p>{stateSearch}</p>
-        <p>{parkSearch}</p>
-    </div>
-  )
+    
+    const baseUrl:string = `https://developer.nps.gov/api/v1/parks?q=${parkSearch}&api_key=${process.env.API_KEY}`
+    const data:parkInfo = await GetData(baseUrl)
+    return (
+      <div>
+        {data.data[0].fullName}
+      </div>
+    )
 }
