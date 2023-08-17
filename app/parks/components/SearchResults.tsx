aba28196061a175273598706c9c520c205e40c0a
Austin Labador <austinlabador@gmail.com>
2023-08-16 23:24:51 -0700

    Create search result cards, pass in data and rework interface for props

--- app/parks/components/SearchResults.tsx
+++ app/parks/components/SearchResults.tsx
@@ -2,7 +2,7 @@ import GetData from "@/api/GetData"
 import SearchCard from "./SearchCard"
 
 
-export interface parkInfo {
+export interface searchInfo {
   total: string
   limit: string
   start: string
@@ -20,14 +20,26 @@ export interface parkInfo {
 
 }
 
+export interface parkInfo {
+  fullName: string,
+  images: [
+    {
+      url: string,
+      altText: string,
+    }
+  ]
+}
+
 export default async function SearchResults({stateSearch, parkSearch}: 
     {stateSearch:string, parkSearch: string}) {
     
     const baseUrl:string = `https://developer.nps.gov/api/v1/parks?q=${parkSearch}&api_key=${process.env.API_KEY}`
-    const data:parkInfo = await GetData(baseUrl)
+    const data:searchInfo = await GetData(baseUrl)
     return (
-      <div>
-        <SearchCard data={data}/>
-      </div>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center py-8 px-8 md:px-12 lg:px-16">
+        {data.data.map((park:parkInfo) => (
+          <SearchCard key={park.fullName} info={park}/>
+        ))}
+      </section>
     )
 }
